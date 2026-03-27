498ed2aefe4de3ba5a6aae4478f30434dcb821d4
matrixportalx <matrixportalx@proton.me>
2026-03-27 23:11:35 +0000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "tr.maya"
         minSdk = 33
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 1
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
